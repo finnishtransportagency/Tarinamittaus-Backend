6188c6cf336bb175fbdab3fe09755e51350102af
Juha Keponen <juha.keponen@cgi.com>
2021-04-22 15:47:19 +0300

    init backend project setup

---
+++ src/main/java/fi/tarina/tarinamittaus/TarinamittausApplication.java
@@ -0,0 +1,13 @@
+package fi.tarina.tarinamittaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarinamittausApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarinamittausApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/tarina/tarinamittaus/TarinamittausApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tarina.tarinamittaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarinamittausApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
