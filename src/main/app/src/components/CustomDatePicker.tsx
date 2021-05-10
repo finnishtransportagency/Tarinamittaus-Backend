dd146291109526e621bad885a220f2da025314b3
Juha Keponen <juha.keponen@cgi.com>
2021-05-10 15:23:03 +0300

    Frontend: Custom fields

---
+++ src/main/app/src/components/CustomDatePicker.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import { fi } from "date-fns/locale";
+import { format, parse, isValid, isDate } from "date-fns";
+import { useField } from "formik";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+
+// isDate() and isValid() both return true
+// for dates with a year of more than 4 digits, but such
+// dates lead to a RangeError in format()
+const isValidDate = (date: Date) => (
+    isDate(date)
+        && isValid(date)
+        && date.getFullYear().toString().length <= 4
+);
+
+export const FormikCustomDatePicker = ({ label, name, readOnly }: {label:string, name:string, readOnly:boolean}) => {
+    const [field, _, helpers] = useField(name);
+    const dateFormat = "yyyy-MM-dd";
+
+    return (
+        <div className="col-sm-12">
+            <label htmlFor={name} className="row">
+                {label}
+            </label>
+            <div className="row">
+                <DatePicker
+                    locale={fi}
+                    dateFormat={dateFormat}
+                    className="tk-field form-control"
+                    disabled={readOnly}
+                    selected={field.value ? parse(field.value, dateFormat, new Date()) : null}
+                    onChange={( date : Date )  => {
+                        const newDate = isValidDate(date) ? format(date, dateFormat) : null;
+                        helpers.setValue(newDate);
+                    }}
+                />
+            </div>
+        </div>
+    );
+};
+
