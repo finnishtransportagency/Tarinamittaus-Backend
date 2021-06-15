a8590c5403b1aa233382e62aea9e83779413d8f6
Juha Keponen <juha.keponen@cgi.com>
2021-06-15 14:07:24 +0300

    servlet configs for spring https://spring.io/blog/2014/03/07/deploying-spring-boot-applications -> "What about the Java EE Application Server?" part

--- src/main/java/fi/tarina/tarinamittaus/TarinamittausApplication.java
+++ src/main/java/fi/tarina/tarinamittaus/TarinamittausApplication.java
@@ -2,12 +2,20 @@ package fi.tarina.tarinamittaus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TarinamittausApplication {
+public class TarinamittausApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TarinamittausApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(TarinamittausApplication.class);
     }
 
 }
