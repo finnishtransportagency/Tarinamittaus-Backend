80331eecb0b2b323be7eec4ca58776e44d40033c
Juha Keponen <juha.keponen@cgi.com>
2021-06-18 09:17:49 +0300

    fix TarinamittausApplication

--- src/main/java/fi/tarina/tarinamittaus/TarinamittausApplication.java
+++ src/main/java/fi/tarina/tarinamittaus/TarinamittausApplication.java
@@ -5,7 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableJpaRepositories
